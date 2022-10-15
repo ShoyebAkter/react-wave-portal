3bd29de8618a81e1ba07f101c9e73c78e781d35d
Shoyeb Akter <heroreal5385@gmail.com>
2022-10-15 08:51:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from "react";
 import './App.css';
+const getEthereumObject=()=>window.ethereum;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const findMetamaskAccount=async()=>{
+  try {
+    const ethereum = getEthereumObject();
+    
+    if (!ethereum) {
+      console.error("Make sure you have Metamask!");
+      return null;
+    }
+
+    console.log("We have the Ethereum object", ethereum);
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      return account;
+    } else {
+      console.error("No authorized account found");
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+  
+}
+export default function App() {
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  /*
+   * The passed callback function will be run when the page loads.
+   * More technically, when the App component "mounts".
+   */
+  useEffect(()=>{
+    const fetchAccount= async() => {
+      const account = await findMetamaskAccount();
+      if (account !== null) {
+        setCurrentAccount(account);
+      }
+      console.log(account);
+    }
+    fetchAccount()
+  }, []);
+  
+  const wave = () => {
+    
+  }
+  
+  return (  
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there!
+        </div>
+
+        <div className="bio">
+        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
